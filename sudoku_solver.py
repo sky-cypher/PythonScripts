f7d7b1ecc8342e327cd4dd3c855927ddf462e62f
Aakash Patil <55867390+sky-cypher@users.noreply.github.com>
2021-10-23 13:35:22 +0530

    added support to read from file

--- sudoku_solver.py
+++ sudoku_solver.py
@@ -1,7 +1,15 @@
 #!/usr/bin/env python3
-from rich import print
-import numpy as np
 from time import time
+try:
+    import numpy as np
+except ImportError:
+    print('Install numpy')
+    quit()
+try:
+    from rich import print
+except ImportError:
+    pass
+
 
 '''
 puzzle = [
@@ -20,6 +28,23 @@ puzzle = [ [ y*0 for y in range(9) ] for x in range(9) ]
 sudoku = np.matrix(puzzle)
 start_time = 0
 end_time = 0
+
+
+def print_sudoku():
+    puzzle = sudoku.tolist()
+    for x in range(9):
+        if x % 3 :
+            print('\n\t', end = '')
+        else:
+            print(f'\n\t{"-" * 23}\n\t', end = '')
+        for y in range(9):
+            if y % 3 :
+                s = ""
+            else:
+                s = "| "
+            print(f"{s}{puzzle[x][y]} ", end = '')
+    print('\n')
+
 
 def check(row, column, value):
     if value in sudoku[row,:]:
@@ -48,8 +73,8 @@ def solve():
                 return
     global end_time, start_time
     end_time = time()
-    print(f"\n[bold green]{sudoku}[/bold green]")
-    if 'n' in input("\ncontinue ? : ").lower():
+    print_sudoku()
+    if 'y' not in input("\nTry to find another solution ? : ").lower():
         quit()
     start_time += time() - end_time
 
@@ -57,6 +82,15 @@ def solve():
 def main():
     global sudoku, start_time
     print(sudoku)
+    if 'n' not in input("Get puzzle from file ? : ").lower():
+        file_path = input("Enter file name : ")
+        if not file_path:
+            file_path = 'sudoku1.txt'
+        with open(file_path) as file:
+            for i, line in enumerate(file.readlines()):
+                puzzle[i] = [ int(x) for x in line.split() ]
+        sudoku = np.matrix(puzzle)
+        print(sudoku)
     while 'n' in input("Use defined sudoku ? : ").lower():
         for i in range(9):
             puzzle[i] = [int(x) for x in input(f"Enter row {i + 1} : ")]
@@ -76,6 +110,11 @@ def main():
         if end_time:
             tat = abs(end_time - start_time)
             print(f"Time Required = {round(tat,4)} seconds")
+        else:
+            print("Invalid puzzle")
 
 if __name__ == "__main__":
-    main()
+    try:
+        main()
+    except KeyboardInterrupt:
+        quit()
