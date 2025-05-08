d7b6aead4805aa65f81b2cb2b0f97cfdedb35dfc
Aakash Patil <55867390+sky-cypher@users.noreply.github.com>
2025-05-08 09:12:40 +0530

    Added Check for regex find

--- mgp_bill_calculator.py
+++ mgp_bill_calculator.py
@@ -3,6 +3,8 @@
 import re
 import sys
 import subprocess
+from rich.console import Console
+from rich.table import Table
 
 if len(sys.argv) != 2:
         print("Check Arguments")
@@ -15,12 +17,22 @@ def calculate_bill(bill, ignore = 0):
         temp *= 100
         temp *= 0.03
         bill += temp
-        return int(bill)
+        global total_commission
+        total_commission += int(temp)
+        return('\u20b9' + str(bill))
+
 
 subprocess.run(
         f"pdftotext {sys.argv[1]} grahak-bill.txt",
         shell=True)
 
+table = Table(title="grahak-bill",show_lines=True)
+
+table.add_column("Name")
+table.add_column("Amount", style="green")
+table.add_column("Bags", style="yellow")
+
+total_commission = 0
 content = ""
 lines = []
 with open("grahak-bill.txt") as file:
@@ -39,16 +51,24 @@ for line in lines:
 
 names = re.findall(r'[A-Z]+\s[A-Z]+\s[A-Z]+', content)
 bags_bill = re.findall(r'[0-9]+\s\|\s[0-9]+\.[0-9]', content)
+if(len(names) != len(bags_bill)):
+        print("Error while parsing: count mismatch")
+        exit(1)
+
 bills = [ x.split()[-1] for x in bags_bill ]
 bags = [ x.split()[0] for x in bags_bill ]
 
-print("Name\tAmount\tBags")
 for name, bill, bag in zip(names, bills, bags):
         if 'AARTI' in name:
                 continue
-        print(''.join( [ x.capitalize() + ' ' for x in name.split()]),
-              calculate_bill(bill),
-              bag,
-              sep='\t'
-              )
+        table.add_row(
+                ''.join(
+                    [ x.capitalize() + ' ' for x in name.split()]
+                ),
+                calculate_bill(bill),
+                bag
+        )
 
+console = Console()
+console.print(table)
+print("\nTotal commission :", total_commission)
